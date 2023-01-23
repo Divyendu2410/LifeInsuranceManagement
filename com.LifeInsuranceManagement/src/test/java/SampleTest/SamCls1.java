c1aed1036d17592c604c5aa7cd8c7eeec8a9cf3f
Administrator <Administrator@DESKTOP-FUGQNF4>
2023-01-23 12:06:17 +0530

    This is my 3rd commit

---
+++ com.LifeInsuranceManagement/src/test/java/SampleTest/SamCls1.java
@@ -0,0 +1,17 @@
+package SampleTest;
+
+import org.testng.annotations.Test;
+
+public class SamCls1 {
+	@Test
+	public void simple1() {
+		System.out.println("-Am Groot-");
+	}
+
+	@Test
+	public void simple2() {
+		System.out.println("--Am Am Groot--");
+	}
+
+
+}
